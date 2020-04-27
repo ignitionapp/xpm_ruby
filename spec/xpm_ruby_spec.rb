804d36c9043970ef5f796dfabe853a2359fda70e
abreckner <skuunk@gmail.com>
2020-04-27 15:19:13 +1000

    rubocop fix

--- spec/xpm_ruby_spec.rb
+++ spec/xpm_ruby_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe(XpmRuby) do
   it "has a version number" do
     expect(XpmRuby::VERSION).not_to be(nil)
   end
-
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
 end
