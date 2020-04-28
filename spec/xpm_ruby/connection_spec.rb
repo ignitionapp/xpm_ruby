de9f8c63bc862995e17b889e87e9d4e691d09fa5
abreckner <skuunk@gmail.com>
2020-04-28 11:21:34 +1000

    Add Faraday post to connection and post test with VCR (#13)
    * Add Faraday post to connection and post test with VCR
    
    * Rubocop fixes

--- spec/xpm_ruby/connection_spec.rb
+++ spec/xpm_ruby/connection_spec.rb
@@ -26,5 +26,30 @@ module XpmRuby
         connection.get(endpoint: "staff.api/list")
       end
     end
+
+    describe "#post" do
+      let(:api_key) { "TEST" }
+      let(:account_key) { "TEST" }
+      let(:api_url) { "api.workflowmax.com" }
+
+      let(:xml_string) do
+        "<Job><Name>Brochure Design</Name><Description>Detailed description of the job</Description><ClientUUID>e349fc1b-d92d-4ba2-b9fc-69fdd516e2a2</ClientUUID><StartDate>20291023</StartDate><DueDate>20291028</DueDate></Job>"
+      end
+
+      around(:each) do |example|
+        VCR.use_cassette("xpm_ruby/connection/post") do
+          example.run
+        end
+      end
+
+      it "should post to Faraday with the right endpoint, data and headers" do
+        connection = service.new(api_key: api_key, api_url: api_url, account_key: account_key)
+        response = connection.post(endpoint: "job.api/add", data: xml_string)
+
+        hash = Ox.load(response.body, mode: :hash_no_attrs, symbolize_keys: false)
+
+        expect(hash["Response"]["Status"]).to eq("OK")
+      end
+    end
   end
 end
