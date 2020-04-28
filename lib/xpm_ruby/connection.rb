0072116af37749d3014889c67e3f8bbc89fd783f
abreckner <skuunk@gmail.com>
2020-04-29 09:30:00 +1000

    Refactor connection to use access token and xero_tenant_id

--- lib/xpm_ruby/connection.rb
+++ lib/xpm_ruby/connection.rb
@@ -3,13 +3,11 @@ require "base64"
 
 module XpmRuby
   class Connection
-    attr_accessor :api_url, :account_key, :api_key, :basic_auth
+    attr_accessor :xero_tenant_id, :authorization
 
-    def initialize(account_key:, api_key:, api_url:)
-      @api_url = api_url
-      @account_key = account_key
-      @api_key = api_key
-      @basic_auth = "Basic " + Base64.strict_encode64("#{api_key}:#{account_key}")
+    def initialize(access_token:, xero_tenant_id:)
+      @xero_tenant_id = xero_tenant_id
+      @authorization = "Bearer " + access_token
     end
 
     def get(endpoint:, params: nil)
@@ -19,18 +17,17 @@ module XpmRuby
 
     def post(endpoint:, data:)
       faraday_connection = Faraday.new(url)
-      puts(headers.merge(content_type: "application/xml"))
-      faraday_connection.post(endpoint, data, headers.merge(content_type: "application/xml"))
+      faraday_connection.post(endpoint, data, headers)
     end
 
     private
 
     def headers
-      { "Authorization" => @basic_auth }
+      { "Authorization" => @authorization, "xero-tenant-id" => @xero_tenant_id, "content_type" => "application/xml"}
     end
 
     def url
-      "https://#{@api_url}/v3/"
+      "https://api.xero.com/practicemanager/3.0/"
     end
   end
 end
