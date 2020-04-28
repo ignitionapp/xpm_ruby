9660507327273157f2d33a55f505854693b68426
Adam Mikulasev <adammikulas@gmail.com>
2020-04-28 16:10:32 +1000

    update based on Tony's feedback

--- lib/xpm_ruby/client.rb
+++ lib/xpm_ruby/client.rb
@@ -1,14 +1,8 @@
-require "faraday"
-require "base64"
-require "ox"
-
-require_relative "client/get"
-require_relative "client/add"
+require_relative "models/client"
 
 module XpmRuby
   module Client
     extend self
-
     module GstPeriod
       ONE = "1"
       TWO = "2"
@@ -43,7 +37,51 @@ module XpmRuby
       PTS = "PTS"
     end
 
-    class Error < StandardError; end
-    class Unauthorized < Error; end
+    def build(**args)
+      Models::Client.new(args)
+    end
+
+    module Contact
+      extend self
+
+      def build(**args)
+        Models::Client::Contact.new(args)
+      end
+    end
+
+    def add(api_key:, account_key:, api_url:, client:)
+      builder = ::Nokogiri::XML::Builder.new do |xml|
+        xml.Client do
+          xml.Name do
+            xml.text(client.name)
+          end
+        end
+      end
+
+      response = Connection
+        .new(api_key: api_key, account_key: account_key, api_url: api_url)
+        .post(endpoint: "client.api/add", data: builder.doc.root.to_xml)
+
+      case response.status
+      when 401
+        hash = Ox.load(response.body, mode: :hash_no_attrs, symbolize_keys: false)
+
+        raise Unauthorized.new(hash["html"]["head"]["title"])
+      when 200
+        hash = Ox.load(response.body, mode: :hash_no_attrs, symbolize_keys: false)
+
+        case hash["Response"]["Status"]
+        when "OK"
+          Client.build(
+            uuid: hash["Response"]["Client"]["UUID"],
+            name: hash["Response"]["Client"]["Name"],
+            email: hash["Response"]["Client"]["Email"])
+        when "ERROR"
+          raise Error.new(response["ErrorDescription"])
+        end
+      else
+        raise Error.new(response.status)
+      end
+    end
   end
 end
