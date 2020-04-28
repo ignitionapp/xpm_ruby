f491a485af39aaa7fc9b8ca4baef136d9d457b18
Adam Mikulasev <adammikulas@gmail.com>
2020-04-28 16:14:17 +1000

    fix ordering

--- lib/xpm_ruby.rb
+++ lib/xpm_ruby.rb
@@ -5,10 +5,9 @@ end
 
 require "nokogiri"
 
-require "xpm_ruby/version"
+require "xpm_ruby/client"
 require "xpm_ruby/connection"
 require "xpm_ruby/job"
 require "xpm_ruby/staff"
-require "xpm_ruby/client"
 require "xpm_ruby/template"
 require "xpm_ruby/version"
