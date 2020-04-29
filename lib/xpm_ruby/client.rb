41d0129daad4b99ad0c7c0d80f277f849c770752
abreckner <skuunk@gmail.com>
2020-04-29 13:25:40 +1000

    rubocop fix

--- lib/xpm_ruby/client.rb
+++ lib/xpm_ruby/client.rb
@@ -49,7 +49,6 @@ module XpmRuby
     end
 
     def add(access_token:, xero_tenant_id:, client:)
-
       response = Connection
         .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
         .post(endpoint: "client.api/add", data: client.to_xml(root: "Client"))
