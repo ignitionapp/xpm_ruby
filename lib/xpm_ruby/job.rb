a2c8a2ecdf4db0a7b7329b9c6f06f0344d592d38
abreckner <skuunk@gmail.com>
2020-04-30 15:51:02 +1000

    removing some code which is not used anymore (#22)

--- lib/xpm_ruby/job.rb
+++ lib/xpm_ruby/job.rb
@@ -6,10 +6,6 @@ module XpmRuby
 
     class Error < Error; end
 
-    def build(**args)
-      Models::Job.new(args)
-    end
-
     def current(access_token:, xero_tenant_id:)
       response = Connection
         .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
