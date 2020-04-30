421137a066e4fb529cad6f0b563c34eefd2681d5
abreckner <skuunk@gmail.com>
2020-04-30 14:24:57 +1000

    removing some code which is not used anymore

--- lib/xpm_ruby/client.rb
+++ lib/xpm_ruby/client.rb
@@ -2,52 +2,6 @@ module XpmRuby
   module Client
     extend self
 
-    module GstPeriod
-      ONE = "1"
-      TWO = "2"
-      SIX = "6"
-    end
-
-    module GstBasis
-      INVOICE = "Invoice"
-      PAYMENT = "Payment"
-      HYBRID = "Hybrid"
-    end
-
-    module ProvisionalTaxBasis
-      STANDARD_OPTION = "Standard Option"
-      ESTIMATE_OPTION = "Estimate Option"
-      RATIO_OPTION = "Ratio Option"
-    end
-
-    module AgencyStatus
-      WITH_EOT = "With EOT"
-      WITHOUT_EOT = "Without EOT"
-      UNLINKED = "Unlinked"
-    end
-
-    module ReturnType
-      IR3 = "IR3"
-      IR3NR = "IR3NR"
-      IR4 = "IR4"
-      IR6 = "IR6"
-      IR7 = "IR7"
-      IR9 = "IR9"
-      PTS = "PTS"
-    end
-
-    module Contact
-      extend self
-
-      def build(**args)
-        Models::Client::Contact.new(args)
-      end
-    end
-
-    def build(**args)
-      Models::Client.new(args)
-    end
-
     def add(access_token:, xero_tenant_id:, client:)
       response = Connection
         .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
