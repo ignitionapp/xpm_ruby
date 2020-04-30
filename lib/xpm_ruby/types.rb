50ee53a74f33914910ce5164c0d39f26c3dd30f2
abreckner <skuunk@gmail.com>
2020-04-30 10:59:31 +1000

    Add dry types gem and Job::Add schema

---
+++ lib/xpm_ruby/types.rb
@@ -0,0 +1,7 @@
+require "dry-types"
+
+module XpmRuby
+  module Types
+    include Dry::Types.module
+  end
+end
---
+++ spec/xpm_ruby/schema/job/add_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+module XpmRuby
+  module Schema
+    RSpec.describe(Job) do
+      context "with a valid Add schema" do
+        it "should not raise an error" do
+          hash = { Name: "Joe Bloggs", Description: "New Job", ClientID: 1234, StartDate: 20091023, DueDate: 20091023 }
+          expect { Job::Add[hash] }.not_to raise_error
+        end
+
+        it "should coerce the types" do
+          hash = { Name: "Joe Bloggs", Description: "New Job", ClientID: 1234, StartDate: 20091023, DueDate: 20091023 }
+          add = Job::Add[hash]
+          expect(add[:ClientID]).to eql("1234")
+        end
+      end
+
+      context "with an invalid Add schema" do
+        it "should raise an error" do
+          hash = { Name: "Joe Bloggs" }
+          expect { Job::Add[hash] }.to raise_error(Dry::Types::MissingKeyError, ":Description is missing in Hash input")
+        end
+      end
+    end
+  end
+end
