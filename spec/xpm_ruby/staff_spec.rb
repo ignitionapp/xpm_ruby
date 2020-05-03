2df6b74cac99c0907c60fbccb0cdbcfc00cc0a10
Adam Mikulasev <adammikulas@gmail.com>
2020-05-04 09:50:37 +1000

    Staff.list - raise AccessToken exceptions (#24)
    * update Connection.get
    
    * add exception handling for Staff.list
    
    * remove ruby-version
    
    * remove access token rejected error

--- spec/xpm_ruby/staff_spec.rb
+++ spec/xpm_ruby/staff_spec.rb
@@ -2,39 +2,48 @@ require "spec_helper"
 
 module XpmRuby
   RSpec.describe(Staff) do
-    subject(:service) { described_class }
-
     describe ".list" do
-      context "when keys invalid" do
-        let(:api_key) { "" }
-        let(:account_key) { "" }
-        let(:api_url) { "api.workflowmax.com" }
+      let(:xero_tenant_id) { "XERO_TENANT_ID" }
 
-        xit "raises unauthorized error" do
-          expect do
-            Staff.list(api_key: api_key, account_key: account_key, api_url: api_url)
-          end.to raise_error(Unauthorized)
+      context "when access token expired" do
+        let(:access_token) { "expired" }
+
+        it "raises access token expired exception" do
+          VCR.use_cassette("xpm_ruby/staff/list/access_token_expired") do
+            expect do
+              Connection
+                .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
+                .get(endpoint: "staff.api/list")
+            end.to raise_error(XpmRuby::AccessTokenExpired)
+          end
         end
       end
 
-      context "when keys valid" do
-        let(:xero_tenant_id) { "XERO_TENANT_ID" }
-        let(:access_token) { "XERO_ACCESS_TOKEN" }
+      context "when access token unauthorized" do
+        let(:access_token) { "unauthorized" }
 
-        around(:each) do |example|
-          VCR.use_cassette("xpm_ruby/staff/list") do
-            example.run
+        it "raises access token expired exception" do
+          VCR.use_cassette("xpm_ruby/staff/list/access_token_unauthorized") do
+            expect do
+              Staff.list(access_token: access_token, xero_tenant_id: xero_tenant_id)
+            end.to raise_error(XpmRuby::Unauthorized)
           end
         end
+      end
+
+      context "when access token accepted" do
+        let(:access_token) { "accepted" }
 
         it "lists staff" do
-          staff_list = service.list(access_token: access_token, xero_tenant_id: xero_tenant_id)
+          VCR.use_cassette("xpm_ruby/staff/list/access_token_accepted") do
+            staff_list = Staff.list(access_token: access_token, xero_tenant_id: xero_tenant_id)
 
-          staff = staff_list.last
+            staff = staff_list.last
 
-          expect(staff["Name"]).to eql("test")
-          expect(staff["Email"]).to eql("adammikulas@gmail.com")
-          # expect(staff.uuid).to eql("1cc99c1e-8cf7-4248-ab84-3e11126facbc")
+            expect(staff["Name"]).to eql("test")
+            expect(staff["Email"]).to eql("adammikulas@gmail.com")
+            # expect(staff.uuid).to eql("1cc99c1e-8cf7-4248-ab84-3e11126facbc")
+          end
         end
       end
     end
