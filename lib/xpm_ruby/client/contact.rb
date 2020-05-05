955060360174e2e7fb05956d548f105eb508d7c4
Adam Mikulasev <adammikulas@gmail.com>
2020-05-05 13:55:34 +1000

    Client::Contact.add IGNT-3571 (#40)
    * add interface
    
    * finish PR
    
    * remove ruby-version
    
    * fix specs

--- lib/xpm_ruby/client/contact.rb
+++ lib/xpm_ruby/client/contact.rb
@@ -3,6 +3,16 @@ module XpmRuby
     module Contact
       extend self
 
+      def add(access_token:, xero_tenant_id:, contact:)
+        validated_contact = Schema::Client::Contact::Add[contact]
+
+        response = Connection
+          .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
+          .post(endpoint: "client.api/contact", data: validated_contact.to_xml(root: "Contact"))
+
+        response["Contact"]
+      end
+
       def delete(access_token:, xero_tenant_id:, contact_id:)
         response = Connection
           .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
