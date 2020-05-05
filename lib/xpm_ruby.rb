c14033592e62774ea7bdc3328b907adcd9ad8a0f
abreckner <skuunk@gmail.com>
2020-05-05 15:17:01 +1000

    Connection error handling improvements (#41)
    * Add error cases for bad token and bad tenant id
    
    * add specs to post for bad token or tenant
    
    * Add error handling to put and delete
    
    * Add comment explaining 403
    
    * 403 now raises Forbidden instead of unauthorised
    
    * extend error not unauthorised

--- lib/xpm_ruby.rb
+++ lib/xpm_ruby.rb
@@ -2,6 +2,7 @@ module XpmRuby
   class Error < StandardError; end
   class Unauthorized < Error; end
   class AccessTokenExpired < Unauthorized; end
+  class Forbidden < Error; end
 end
 
 require "active_support"
