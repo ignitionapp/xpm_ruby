03be55a9314c28c4c4bc3b7728a45b92a5d35f7d
Adam Mikulasev <adammikulas@gmail.com>
2020-05-05 15:09:47 +1000

    Client::Contact.update IGNT-3572 (#42)
    * finish PR
    
    * add missing files

--- lib/xpm_ruby/client/contact.rb
+++ lib/xpm_ruby/client/contact.rb
@@ -13,6 +13,18 @@ module XpmRuby
         response["Contact"]
       end
 
+      def update(access_token:, xero_tenant_id:, id:, contact:)
+        validated_contact = Schema::Client::Contact::Update[contact]
+
+        response = Connection
+          .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
+          .put(
+            endpoint: "client.api/contact/#{id}",
+            data: validated_contact.to_xml(root: "Contact"))
+
+        response["Contact"]
+      end
+
       def delete(access_token:, xero_tenant_id:, contact_id:)
         response = Connection
           .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
