d8ec736e069bc44103d6a407613ea1b7b4db57b8
Adam Mikulasev <adammikulas@gmail.com>
2020-05-05 15:09:47 +1000

    Client::Contact.update IGNT-3572 (#42)
    * finish PR
    
    * add missing files

---
+++ spec/xpm_ruby/schema/client/contact/update_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+module XpmRuby
+  module Schema
+    module Client
+      RSpec.describe(Contact) do
+        context "with a valid Update schema" do
+          it "should not raise an error" do
+            hash = { "Name" => "Joe Bloggs" }
+            expect { Contact::Update[hash] }.not_to raise_error
+          end
+        end
+      end
+    end
+  end
+end
