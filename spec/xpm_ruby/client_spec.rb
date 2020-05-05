30707296c9702b8b49a31f6266dd72aa0134f256
Adam Mikulasev <adammikulas@gmail.com>
2020-05-05 16:54:55 +1000

    add specs (incomplete)

--- spec/xpm_ruby/client_spec.rb
+++ spec/xpm_ruby/client_spec.rb
@@ -42,15 +42,56 @@ module XpmRuby
 
     describe ".list" do
       let(:xero_tenant_id) { "0791dc22-8611-4c1c-8df7-1c5453d0795b" }
-      let(:access_token) { "token" }
+      let(:access_token) { "" }
 
-      it "list client" do
-        VCR.use_cassette("xpm_ruby/client/list") do
-          clients = Client.list(
-            access_token: access_token,
-            xero_tenant_id: xero_tenant_id)
+      context "when not modified since" do
+        it "list all clients" do
+          VCR.use_cassette("xpm_ruby/client/list/when_not_modified_since") do
+            clients = Client.list(access_token: access_token, xero_tenant_id: xero_tenant_id)
 
-          expect(clients.count).to eq(12)
+            expect(clients.count).to eq(12)
+          end
+        end
+      end
+
+      context "when modified since" do
+        let(:modified_since) { "2020-05-05T06:24:31" }
+
+        it "list clients modified since" do
+          VCR.use_cassette("xpm_ruby/client/list/when_modified_since") do
+            clients = Client.list(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              modified_since: modified_since)
+
+            expect(clients.count).to eq(36)
+          end
+        end
+      end
+
+      context "when detailed false" do
+        it "list clients without details" do
+          VCR.use_cassette("xpm_ruby/client/list/with_detailed_false") do
+            clients = Client.list(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              detailed: false)
+
+            expect(clients.count).to eq(12)
+          end
+        end
+      end
+
+      context "when detailed true" do
+        it "list clients with details" do
+          VCR.use_cassette("xpm_ruby/client/list/with_detailed_true") do
+            clients = Client.list(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              detailed: true)
+
+            expect(clients.count).to eq(12)
+          end
         end
       end
     end
