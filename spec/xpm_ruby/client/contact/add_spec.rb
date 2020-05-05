6a804282552b277748357a5f487f176d6c95aca2
Adam Mikulasev <adammikulas@gmail.com>
2020-05-05 13:55:34 +1000

    Client::Contact.add IGNT-3571 (#40)
    * add interface
    
    * finish PR
    
    * remove ruby-version
    
    * fix specs

---
+++ spec/xpm_ruby/client/contact/add_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+module XpmRuby
+  module Client
+    RSpec.describe(Contact) do
+      describe ".add" do
+        let(:xero_tenant_id) { "XERO_TENANT_ID" }
+        let(:access_token) { "token" }
+
+        let(:contact) do
+          {
+            "Client" => { "ID" => 25655881 },
+            "Name" => "Acmer Pty Ltd"
+          }
+        end
+
+        it "adds contact" do
+          VCR.use_cassette("xpm_ruby/client/contact/add") do
+            created_contact = Contact.add(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              contact: contact)
+
+            expect(created_contact["ID"]).not_to be_nil
+            expect(created_contact["Name"]).to eql(created_contact["Name"])
+          end
+        end
+      end
+    end
+  end
+end
