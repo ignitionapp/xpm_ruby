e284af34000b1f1e9b216be230084c984b8b8270
abreckner <skuunk@gmail.com>
2020-05-06 13:31:15 +1000

    Add applytemplate endpoint to Job (#48)
    * Add applytemplate schema
    
    * add schema
    
    * Add applytemplate specs
    
    * small fixes

---
+++ lib/xpm_ruby/schema/job/applytemplate.rb
@@ -0,0 +1,13 @@
+require_relative "../../types"
+
+module XpmRuby
+  module Schema
+    module Job
+      Applytemplate = Types::Hash.schema(
+        ID: Types::Coercible::String,
+        TemplateID: Types::Coercible::String,
+        TaskMode?: Types::String
+      ).with_key_transform(&:to_sym)
+    end
+  end
+end
