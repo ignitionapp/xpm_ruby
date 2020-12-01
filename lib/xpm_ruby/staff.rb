3b4f787e6eac23579f171ae0dd1af1150a49772a
Walid Halabi <whalabi@gmail.com>
2020-12-02 10:41:44 +1100

    allow the base url to be overwritten to query workflowmax where needed

--- lib/xpm_ruby/staff.rb
+++ lib/xpm_ruby/staff.rb
@@ -6,9 +6,9 @@ module XpmRuby
 
     class Error < Error; end
 
-    def list(access_token:, xero_tenant_id:)
+    def list(access_token:, xero_tenant_id:, url: nil)
       response = Connection
-        .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
+        .new(access_token: access_token, xero_tenant_id: xero_tenant_id, url: url)
         .get(endpoint: "staff.api/list")
 
       Array.wrap(response["StaffList"]["Staff"])
