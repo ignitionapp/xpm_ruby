70117f55bc029208e70c0350c08619bcb4b754ee
Chris Nelson <git@chrisnelson.io>
2021-03-31 13:14:52 +1100

    Add staff get method

--- lib/xpm_ruby/staff.rb
+++ lib/xpm_ruby/staff.rb
@@ -6,6 +6,14 @@ module XpmRuby
 
     class Error < Error; end
 
+    def get(access_token:, xero_tenant_id:, staff_id:)
+      response = Connection
+        .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
+        .get(endpoint: "staff.api/get/" + staff_id)
+
+      response["Staff"]
+    end
+
     def list(access_token:, xero_tenant_id:, url: nil)
       response = Connection
         .new(access_token: access_token, xero_tenant_id: xero_tenant_id, url: url)
