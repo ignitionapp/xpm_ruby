a5b01ce8bc7e7983058e7c3174f87e691c21028c
Ramil <ramustafin@gmail.com>
2023-04-14 21:13:56 +1000

    [RB-326] add spec & vcr cassette

--- spec/xpm_ruby/contact_spec.rb
+++ spec/xpm_ruby/contact_spec.rb
@@ -30,22 +30,47 @@ module XpmRuby
       let(:xero_tenant_id) { "0791dc22-8611-4c1c-8df7-1c5453d0795b" }
       let(:access_token) { "token" }
 
-      let(:contact) do
-        {
-          "Name" => "Acmer Pty Ltd Updated 3"
-        }
+      
+      context "without Client ID" do
+        let(:contact) do
+          {
+            "Name" => "Acmer Pty Ltd Updated 3"
+          }
+        end
+
+        it "updates contact" do
+          VCR.use_cassette("xpm_ruby/contact/update") do
+            updated_contact = Contact.update(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              id: 15412877,
+              contact: contact)
+
+            expect(updated_contact["ID"]).not_to be_nil
+            expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+          end
+        end
       end
 
-      it "updates contact" do
-        VCR.use_cassette("xpm_ruby/contact/update") do
-          updated_contact = Contact.update(
-            access_token: access_token,
-            xero_tenant_id: xero_tenant_id,
-            id: 15412877,
-            contact: contact)
+      context "with Client ID" do
+        let(:contact) do
+          {
+            "Client" => { "ID" => 25655881 },
+            "Name" => "Acmer Pty Ltd Updated 3"
+          }
+        end
 
-          expect(updated_contact["ID"]).not_to be_nil
-          expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+        it "updates contact" do
+          VCR.use_cassette("xpm_ruby/contact/update") do
+            updated_contact = Contact.update(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              id: 15412877,
+              contact: contact)
+
+            expect(updated_contact["ID"]).not_to be_nil
+            expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+          end
         end
       end
     end
