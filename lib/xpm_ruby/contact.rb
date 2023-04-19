ee0753ca3e9b6652c4752ad9b6e081acc383961d
Ramil <rommel.rmm@gmail.com>
2023-04-19 13:13:30 +0800

    [RB-326] v31 client updates (#73)

--- lib/xpm_ruby/contact.rb
+++ lib/xpm_ruby/contact.rb
@@ -24,10 +24,12 @@ module XpmRuby
       response["Contact"]
     end
 
-    def delete(access_token:, xero_tenant_id:, contact_id:)
+    def delete(access_token:, xero_tenant_id:, contact_id:, client_id: nil)
+      request_params = { client_id: client_id }.compact
+
       response = Connection
         .new(access_token: access_token, xero_tenant_id: xero_tenant_id)
-        .delete(endpoint: "client.api/contact", id: contact_id)
+        .delete(endpoint: "client.api/contact", id: contact_id, params: request_params)
 
       response["Contact"]
     end
