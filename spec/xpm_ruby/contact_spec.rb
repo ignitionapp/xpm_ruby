5e75767246bcebc9dc8955ae59582491597cddd9
Ramil <rommel.rmm@gmail.com>
2023-04-19 13:13:30 +0800

    [RB-326] v31 client updates (#73)

--- spec/xpm_ruby/contact_spec.rb
+++ spec/xpm_ruby/contact_spec.rb
@@ -30,22 +30,46 @@ module XpmRuby
       let(:xero_tenant_id) { "XERO_TENANT_ID" }
       let(:access_token) { "token" }
 
-      let(:contact) do
-        {
-          "Name" => "Acmer Pty Ltd Updated 3"
-        }
+      context "without Client ID" do
+        let(:contact) do
+          {
+            "Name" => "Acmer Pty Ltd Updated 3"
+          }
+        end
+
+        it "updates contact" do
+          VCR.use_cassette("xpm_ruby/contact/update") do
+            updated_contact = Contact.update(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              id: 15412877,
+              contact: contact)
+
+            expect(updated_contact["ID"]).not_to be_nil
+            expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+          end
+        end
       end
 
-      it "updates contact" do
-        VCR.use_cassette("xpm_ruby/contact/update") do
-          updated_contact = Contact.update(
-            access_token: access_token,
-            xero_tenant_id: xero_tenant_id,
-            id: 15412877,
-            contact: contact)
+      context "with Client ID" do
+        let(:contact) do
+          {
+            "Client" => { "ID" => 25655881 },
+            "Name" => "Acmer Pty Ltd Updated 3"
+          }
+        end
 
-          expect(updated_contact["ID"]).not_to be_nil
-          expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+        it "updates contact" do
+          VCR.use_cassette("xpm_ruby/contact/update") do
+            updated_contact = Contact.update(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              id: 15412877,
+              contact: contact)
+
+            expect(updated_contact["ID"]).not_to be_nil
+            expect(updated_contact["Name"]).to eql(updated_contact["Name"])
+          end
         end
       end
     end
@@ -55,15 +79,32 @@ module XpmRuby
       let(:access_token) { "token" }
       let(:contact_id) { "14574323" }
 
-      it "deletes the contact" do
-        VCR.use_cassette("xpm_ruby/contact/delete") do
-          deleted_contact = Contact.delete(
-            access_token: access_token,
-            xero_tenant_id: xero_tenant_id,
-            contact_id: contact_id)
+      context "without Client ID" do
+        it "deletes the contact" do
+          VCR.use_cassette("xpm_ruby/contact/delete") do
+            deleted_contact = Contact.delete(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              contact_id: contact_id)
 
-          expect(deleted_contact["ID"]).to eql(contact_id)
-          expect(deleted_contact["Name"]).to eql("some guy person")
+            expect(deleted_contact["ID"]).to eql(contact_id)
+            expect(deleted_contact["Name"]).to eql("some guy person")
+          end
+        end
+      end
+
+      context "with Client ID" do
+        it "deletes the contact" do
+          VCR.use_cassette("xpm_ruby/contact/delete") do
+            deleted_contact = Contact.delete(
+              access_token: access_token,
+              xero_tenant_id: xero_tenant_id,
+              contact_id: contact_id,
+              client_id: 25655881)
+
+            expect(deleted_contact["ID"]).to eql(contact_id)
+            expect(deleted_contact["Name"]).to eql("some guy person")
+          end
         end
       end
     end
--- spec/xpm_ruby/schema/contact/update_spec.rb
+++ spec/xpm_ruby/schema/contact/update_spec.rb
@@ -8,6 +8,13 @@ module XpmRuby
           hash = { "Name" => "Joe Bloggs" }
           expect { Contact::Update[hash] }.not_to raise_error
         end
+
+        context "with a client ID" do
+          it "should not raise an error" do
+            hash = { "Client" => { "ID" => "123" }, "Name" => "Joe Bloggs" }
+            expect { Contact::Update[hash] }.not_to raise_error
+          end
+        end
       end
     end
   end
